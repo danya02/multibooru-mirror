28233cc1506c7c7edc743998cf63dddec6c2acbc
Danya Generalov <danya@danya02.ru>
2022-08-25 11:48:54 +0300

    Fixing ReadTheDocs again.

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'multibooru-mirror'
+copyright = '2022, danya02'
+author = 'danya02'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+root_doc = 'index.rst_1'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,11 +1,20 @@
-multibooru-mirror
-=================
+.. multibooru-mirror documentation master file, created by
+   sphinx-quickstart on Thu Aug 25 11:47:54 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-Record data on multiple booru-like sites.
-
-Contents
---------
+Welcome to multibooru-mirror's documentation!
+=============================================
 
 .. toctree::
+   :maxdepth: 2
+   :caption: Contents:
 
-    format
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
